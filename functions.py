5229dcb14520b39f9d0897d3d28bae78b36e1862
AndreDulla <77928062+AndreDulla@users.noreply.github.com>
2023-11-24 22:34:37 +0100

    to_strings asks for presents

--- functions.py
+++ functions.py
@@ -1,12 +1,34 @@
-def to_string(f):
+def ask_present():
+    presents = []
+    y = 1
+    while 1:
+        user = int(input(f"Presente {y}: "))
+        if user == 0:
+            break
+        if user != '':
+            presents.append(int(user))
+            print(presents)
+        y += 1
+
+    return presents
+
+
+def to_string(f, presents):
     concat_strings = ""
+    separator = ""
+
     while 1:
         line = f.readline()
+
         if line == "":
             return concat_strings
-        line = line.replace("\n", "")
-        line = line[2:]
-        concat_strings += line + ", "
+        for x in range(len(presents)):
+            if int(line[0]) == presents[x - 1]:
+                line = line.replace("\n", "")
+                line = line[2:]
+                concat_strings += separator + line
+                separator = ", "
+                break
 
 
 def add_mail():
