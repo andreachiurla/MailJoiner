61bb4ebfaed9d10c9032bb38b9b1db21ca7ad2f2
AndreDulla <77928062+AndreDulla@users.noreply.github.com>
2023-11-25 12:56:55 +0100

    Implementing function search_in_file

--- functions.py
+++ functions.py
@@ -5,17 +5,18 @@ underline = '\u001b[4m'
 yellow = "\033[93m"
 reset = "\033[0m"
 
+
 def ask_present():
     presents = []
     y = 1
     while 1:
-        user = int(input(f"Presente {y}: "))
-        if user == 0:
+        user = input(f"Presente {y}: ")
+        if user == '0':
             break
         if user != '':
             presents.append(int(user))
             print(presents)
-        y += 1
+            y += 1
 
     return presents
 
@@ -30,7 +31,6 @@ def to_string(f, presents):
         if line == "":
             return concat_strings
 
-        line = search_in_file(f, line[0])
         for present in presents:
             if int(line[0]) == present:
                 line = line.replace("\n", "")
@@ -48,16 +48,23 @@ def add_mail(f):
         mail_to_add = input(yellow + "Inserisci un indirizzo mail: " + reset)
         if mail_to_add == "":
             break
-        n_address = input(yellow + "Inserisci il numero identificativo: " + reset)
-        if is_free(f, n_address):
-            address_list.append("\n" + mail_to_add)
-            print(address_list)
-        else:
-            print(red, "Numero già utilizzato", reset)
-
+        while 1:
+            n_address = input(yellow + "Inserisci il numero identificativo: " + reset)
+            if not search_in_file(f, n_address):    # if the number hasn't been used yet
+                address_list.append(n_address + " " + mail_to_add + "\n")
+                break
+            else:
+                print(red, "Numero già utilizzato", reset)
 
     f.writelines(address_list)
 
 
+# Searches int in a file
+# If 'to_search' is in the first position of the line returns True, otherwise False
 def search_in_file(f, to_search):
-    return "linea file"
+    while 1:
+        line = f.readline()
+        if line == "":
+            return False
+        if int(line[0]) == to_search:
+            return True
